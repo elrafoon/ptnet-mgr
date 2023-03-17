37046b3ad09ed4f1624f1a3f09f9ceb3227f33af
Stanislav Ravas <ravas@tind.sk>
2023-03-17 09:35:31 +0100

    NodeScanProcess: Query TI232

--- src/nodescan_process.rs
+++ src/nodescan_process.rs
@@ -53,20 +53,62 @@ impl<'a> NodeScanProcess<'a> {
     async fn scan(&mut self, node: &NodeRecord) -> Result<(), Box<dyn std::error::Error>> {
         info!("Scan node {}", node.mac());
 
-        let msg = Message {
-            port: PORT_AUTO,
-            header: Header {
-                C: (BIT_PRM | FC_PRM_SEND_NOREPLY) as u8,
-                address: node.address,
-            },
-            payload: vec![10],
-        };
+        let msg;
+        {
+            let mut buf = packet::buffer::Dynamic::new();
+            PtNetPacket::with_asdh(&ptnet_c::ASDH::with(0, COT::REQ, false), &mut buf)?
+                .begin_asdu(&ptnet_c::DUI::with_direct(ptnet_c::TC_C_RD, 1, false))?
+                .add_ioa(0)?
+                .end_asdu()?;
+
+            msg = Message {
+                port: PORT_AUTO,
+                header: ptnet_c::Header {
+                    C: (BIT_PRM | FC_PRM_SEND_NOREPLY) as u8,
+                    address: node.address,
+                },
+                payload: buf.into(),
+            };
+
+        }
 
         debug!("Transmit request");
         let rcvr = self.sender.send_message(&msg).await?;
-        debug!("Await response");
+
+        debug!("Await request result");
         let result = rcvr.await?;
-        debug!("result = {result}");
+        debug!("result = {}", result);
+
+        let rsp: Message;
+        {
+            let timeout = sleep(Duration::from_secs(5));
+            tokio::pin!(timeout);
+            'rsp_loop: loop {
+                select! {
+                    msg = self.message_rcvr.recv() => {
+                        rsp = msg?;
+                        debug!("Some response arrived");
+
+                        if rsp.header.prm() && rsp.header.address == node.address {
+                            if let Some(fc) = rsp.header.fc() {
+                                match fc {
+                                    FC::PrmSendConfirm | FC::PrmSendNoreply => { break 'rsp_loop; },
+                                    _ => {}
+                                }
+                            }
+                        }
+                        break;
+                    },
+                    _ = &mut timeout => {
+                        warn!("Response timed out!");
+                        return Ok(());
+                    }
+                }
+            }
+        }
+
+        info!("Matching response arrived");
+
         Ok(())
     }
 }
