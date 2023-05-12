6305d69c16696be433d9ec8a04a9edd0dcb8ecda
Stanislav Ravas <ravas@tind.sk>
2023-05-12 08:45:06 +0200

    sol-lib: Move to standalone repo

--- sol-lib/src/helpers.rs
+++
@@ -1,9 +0,0 @@
-use std::mem::size_of;
-
-pub unsafe fn any_as_u8_slice<'a, T: Sized>(p: &'a T) -> &'a [u8] {
-    ::std::slice::from_raw_parts((p as *const T) as *const u8, size_of::<T>())
-}
-
-pub unsafe fn any_as_u8_slice_mut<'a, T: Sized>(p: &'a mut T) -> &'a mut [u8] {
-    ::std::slice::from_raw_parts_mut((p as *mut T) as *mut u8, size_of::<T>())
-}
--- sol-lib/src/lib.rs
+++
@@ -1,2 +0,0 @@
-pub mod image_header;
-pub mod helpers;
